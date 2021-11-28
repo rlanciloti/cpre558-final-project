b6aaf2ffa9b57e29032348057c4af589f1624be1
Ryan Lanciloti <ryanjl9@iastate.edu>
2021-11-27 23:54:42 -0600

    This should fix the failed merging

--- src/test.py
+++ src/test.py
@@ -1,4 +1,13 @@
+"""
+File: test.py
 
+This file will act as the entry point for our project while testing. More detail to come
+as the project is expanded.
+
+Creation Date: 11/27/21
+Last Modified: 11/27/21
+Version: 1.2
+"""
 
 import sys
 import simsogui
