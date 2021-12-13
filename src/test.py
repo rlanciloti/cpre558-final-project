87ed6a528a79a70e6fa0ba2be17962c96799b349
Ryan Lanciloti <ryanjl9@iastate.edu>
2021-12-13 10:57:15 -0600

    Added another test

--- src/test.py
+++ src/test.py
@@ -5,7 +5,7 @@ This file will act as the entry point for our project while testing. More detail
 as the project is expanded.
 
 Creation Date: 11/27/21
-Last Modified: 11/28/21
+Last Modified: 12/13/21
 Version: 1.3
 """
 
@@ -27,11 +27,16 @@ def main(argv: list) -> None:
 	"""
 
 	tasks = [
-		Job("T1", 0, 28, 80),
-		Job("T2", 0, 24, 73),
-		Job("T3", 14, 39, 95),
-		Job("T4", 5, 25, 89),
-		Job("T5", 35, 21, 108)
+		# Job("T1", 0, 28, 80),
+		# Job("T2", 0, 24, 73),
+		# Job("T3", 14, 39, 95),
+		# Job("T4", 5, 25, 89),
+		# Job("T5", 35, 21, 108)
+		Job("T1", 0, 6, 10),
+		Job("T2", 2, 7, 13),
+		Job("T3", 1, 3, 16),
+		Job("T4", 2, 7, 17),
+		Job("T5", 3, 9, 19)
 	]
 
 	proc_list = [
@@ -41,8 +46,12 @@ def main(argv: list) -> None:
 
 	scheduler = myopic_scheduler(tasks, proc_list, 3, 1, 3)
 
-	for proc in scheduler.proc_list:
-		print(proc.get_task_list())
+	if scheduler.done:
+		for proc in scheduler.proc_list:
+			print(proc.get_task_list())
+
+	else:
+		print("Failed to find viable schedule")
 
 	if len(argv) == 1:
 		return
