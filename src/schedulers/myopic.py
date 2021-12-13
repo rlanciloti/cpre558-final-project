cf70034a1305741846ebca3d26b60b494239b967
Ryan Lanciloti <ryanjl9@iastate.edu>
2021-12-12 22:57:36 -0600

    Wrote my own myopic scheduler

--- src/schedulers/myopic.py
+++ src/schedulers/myopic.py
@@ -8,51 +8,117 @@ Assumptions:
 Offline scheduling algorithm so all task information is known apriori
 """
 
-from simso.core import Job
-from simso.core.Processor import Processor
-from simso.core import Scheduler
-from typing import Tuple
+from schedulers.resources.Job import Job
+from schedulers.resources.Processor import Processor
 
-NUM_PROCS = 2
 
-class myopic_scheduler (Scheduler):
+class myopic_scheduler:
+	"""
+	Class: myopic_scheduler
 
-	def __init__(self, task_list: list, window: int, w_value: int, max_backtracks: int) -> None:
+	This class is a multiprocessor scheudler that will add tasks to list of available
+	processors when possible.
+	"""
+
+	def __init__(
+		self, task_list: list, proc_list: list, window: int, w_value: int,
+		max_backtracks: int) -> None:
+		"""
+		Function: __init__
+
+		This function is the constructor for a myopic scheduler
+
+		:param task_list: List of tasks to be scheduled
+		:type task_list: list
+		:param proc_list: List of processors available to scheudle on
+		:type proc_list: list
+		:param window: Number of tasks to be considered when scheduling
+		:type window: int
+		:param w_value: Weigth for the heuristic function
+		:type w_value: int
+		:param max_backtracks: Maximum number of back tracks allowed before giving up
+		:type max_backtracks: int
+		"""
 		self.task_list = task_list
 		self.task_order = list()
-		self.proc_list = [list() for x in range(NUM_PROCS)]
+		self.proc_list = proc_list
 		self.w_value = w_value
 		self.window = window
 		self.max_backtracks = max_backtracks
 		self.done = False
 		self.failed = False
-		self.proc_avail_list = [[0] for x in self.proc_list]
 
-	def init(self) -> None:
-		pass
+		self.create_schedule()
 
-	def on_activate(self, job: Job) -> None:
-		pass
+	def get_next_avail_proc(self) -> Processor:
+		"""
+		Function: get_next_available_proc
 
-	def on_terminated(self, job: Job) -> None:
-		pass 
+		This function gets the next processor which can be scheduled on.
 
-	def schedule(self, cpu: Processor) -> Tuple:
-		pass
-	
-	def create_schedule(self):
-		self.task_list.sort(key=lambda x: x.absolute_deadline)
+		:return: Next free processor
+		:rtype: Processor
+		"""
+		min_time = min([proc.next_available_time for proc in self.proc_list])
+		proc_l = [
+			proc for proc in self.proc_list
+			if proc.next_available_time == min_time
+		]
+
+		return proc_l[0] if len(proc_l) > 0 else None
+
+	def create_schedule(self) -> None:
+		"""
+		Function: create_schedule
+
+		This function will call a recursive function which will build a scheudle as per the
+		myopic scheduler rules
+		"""
+		self.task_list.sort(key=lambda x: x.deadline)
 		self.create_schedule_helper(self.task_list)
-		
+
 	def feasibility_function(self, job: Job) -> bool:
-		return max(job.start_date, min([x[-1] for x in self.proc_avail_list])) <= job.absolute_deadline
-		
-	def heuristic_function(self, job: Job, w_value: int) -> int:
-		EST = max(job.start_date, min([x[-1] for x in self.proc_avail_list]))
-		h = job.absolute_deadline + w_value * EST
+		"""
+		Function: feasibility_function
+
+		This function will determine if a give task is feasible or not
+
+		:param job: Job to be considered
+		:type job: Job
+		:return: True if the job is feasible
+		:rtype: bool
+		"""
+		return max(
+			job.arrival_t,
+			min(
+				[proc.next_available_time for proc in self.proc_list]
+			)
+		) <= job.deadline
+
+	def heuristic_function(self, job: Job) -> int:
+		"""
+		Function: heuristic_function
+
+		This function is used for determining which job is the best option to schedule
+
+		:param job: Job which needs to be scheduled
+		:type job: Job
+		:return: The provided job's heuristic value
+		:rtype: int
+		"""
+		EST = max(job.arrival_t, min([proc.next_available_time for proc in self.proc_list]))
+		h = job.deadline + self.w_value * EST
 		return h
 
-	def create_schedule_helper(self, jobs: list(Job)):
+	def create_schedule_helper(self, jobs: list) -> None:
+		"""
+		Function: create_schedule_helper
+
+		This function recursively builds out a schedule as per myopic scheduler rules
+
+		:param jobs: List of jobs to be scheduled
+		:type jobs: list
+		"""
 		if self.max_backtracks < 0:
 			self.failed = True
 			return
@@ -62,32 +128,36 @@ class myopic_scheduler (Scheduler):
 			return
 
 		sub_jobs = jobs[0:self.window]
-		feasability = [self.feasability_function(job) for job in sub_jobs]
+		feasability = [self.feasibility_function(job) for job in sub_jobs]
 
 		if not feasability and jobs:
 			self.max_backtracks -= 1
 			return
 
 		heuristics = [
-			(self.heuristic_function(
-				sub_jobs[job],
-				self.w_value
-			), job) for job in range(sub_jobs) if feasability[job]
+			(
+				self.heuristic_function(
+					sub_jobs[job]
+				),
+				job
+			) for job in range(len(sub_jobs)) if feasability[job]
 		]
 
 		if heuristics:
 			heuristics.sort(key=lambda x: x[0])
-			next_avail_proc = min([proc for proc in self.proc_avail_list], key=lambda x: x[-1])
 
 			for job in heuristics:
-				j = jobs.pop(job)
+				j = jobs.pop(job[1])
+				next_avail_proc: Processor = self.get_next_avail_proc()
+				assert next_avail_proc is not None
+
+				next_avail_proc.add_job(j)
 				self.create_schedule_helper(jobs)
 
 				if self.done:
 					return
 
 				jobs.append(j)
-				jobs.sort(key=lambda x: x.absolute_deadline)
+				jobs.sort(key=lambda x: x.deadline)
 		else:
 			self.max_backtracks -= 1
-			return jobs
--- src/test.py
+++ src/test.py
@@ -10,9 +10,9 @@ Version: 1.3
 """
 
 import sys
-import simsogui
-
-from SimBuilder import SimBuilder
+from schedulers.resources.Job import Job
+from schedulers.resources.Processor import Processor
+from schedulers.myopic import myopic_scheduler
 
 
 def main(argv: list) -> None:
@@ -26,18 +26,27 @@ def main(argv: list) -> None:
 	:type argv: list
 	"""
 
+	tasks = [
+		Job("T1", 0, 28, 80),
+		Job("T2", 0, 24, 73),
+		Job("T3", 14, 39, 95),
+		Job("T4", 5, 25, 89),
+		Job("T5", 35, 21, 108)
+	]
+
+	proc_list = [
+		Processor("P1"),
+		Processor("P2")
+	]
+
+	scheduler = myopic_scheduler(tasks, proc_list, 3, 1, 3)
+
+	for proc in scheduler.proc_list:
+		print(proc.get_task_list())
+
 	if len(argv) == 1:
 		return
-
-	sim = SimBuilder(argv[1])
-	sim.run_model()
-
-	model = sim.model
-
-	for log in model.logs:
-		print(log)
 
 
 if __name__ == '__main__':
 	main(sys.argv)
-	simsogui.run_gui()
